3121d8e6db6443fbcd28e346d57c2286b24cbf14
Mizushino0904 <157794791+Mizushino0904@users.noreply.github.com>
2024-07-18 15:30:30 +0900

    Add files via upload

---
+++ 21_banner/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+    //
+    var bannerArray = ["red","yellow","green","blue","brown"];
+
+    //
+    var num = Math.random();
+
+    //
+    num = num * 5;
+
+    //
+    num = Math.floor(num);
+
+    //
+    $("aside img").attr("src","img/" + bannerArray[num] + ".jpg");
+});
